8fb2614e81e5f288abfc218b2c9db684ef22f72c
Raiyaan Rahman <crazy@macbook-air.home>
2021-10-27 02:06:55 -0400

    added game icons on homepage

---
+++ app/src/components/GameIcon.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const GameIcon = (props) => {
+    
+    return (
+        <div className="game-icon">
+            <div className="sample-image"> Sample Image </div>
+            <div className="info">
+                <div className="title-publisher">
+                    <h4> Title </h4>
+                    <h5 className="publisher"> Publisher </h5>
+                </div>
+                <div className="percent">
+                    <h4 className={props.percentColour}> {props.percent}% </h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GameIcon;
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import GameIcon from './GameIcon';
 
 const Home = (props) => {
+
+    // We would get this list from the backend
+    const NUM_GAME_ICONS = 20;
+    let game_icons = [];
+
+    for(let i = 0; i < NUM_GAME_ICONS; i++){
+        let percent = (Math.random()*100).toFixed(2);
+        let colour;
+        if(percent < 50){
+            colour = "red-percent";
+        }
+        else if(percent < 75){
+            colour = "yellow-percent";
+        }
+        else {
+            colour = "green-percent";
+        }
+        game_icons.push(<GameIcon percent={percent} percentColour={colour}/>);
+    }
     
     return (
         <div>
-            This is the homepage
+            <div>This is the homepage</div>
+            {game_icons}
         </div>
     );
 };
