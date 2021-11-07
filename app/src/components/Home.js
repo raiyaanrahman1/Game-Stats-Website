2f5a37b49c11a72f61be53cc18780e9b4fc0c559
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2021-11-07 17:19:35 -0500

    fixed bugs yay

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import GameIcon from './GameIcon';
 //import {useEffect} from 'react';
+let gamesSet = false;
+let game_icons = [];
 
 const Home = (props) => {
     
     // We would get this list from the backend
     const NUM_GAME_ICONS = 20;
-    let game_icons = [];
+    
     let games = [];
 
-    for(let i = 0; i < NUM_GAME_ICONS; i++){
-        let percent = (Math.random()*100).toFixed(2);
-        let colour;
-        if(percent < 50){
-            colour = "red-percent";
+    if(!gamesSet){
+        for(let i = 0; i < NUM_GAME_ICONS; i++){
+            let percent = (Math.random()*100).toFixed(2);
+            let colour;
+            if(percent < 50){
+                colour = "red-percent";
+            }
+            else if(percent < 75){
+                colour = "yellow-percent";
+            }
+            else {
+                colour = "green-percent";
+            }
+            let gameID = (Math.random()*10000).toFixed(0);
+            game_icons.push(<GameIcon gameID={gameID} gameName={"Game " + (i+1)} size="game-icon-regular" percent={percent} percentColour={colour} key={i}/>);
+            games.push({gameName:"Game " + (i+1), gameID:gameID});
         }
-        else if(percent < 75){
-            colour = "yellow-percent";
-        }
-        else {
-            colour = "green-percent";
-        }
-        let gameID = (Math.random()*10000).toFixed(0);
-        game_icons.push(<GameIcon gameID={gameID} gameName={"Game " + (i+1)} size="game-icon-regular" percent={percent} percentColour={colour} key={i}/>);
-        games.push({gameName:"Game " + (i+1), gameID:gameID});
+        props.setGames(games);
+        gamesSet = true;
     }
-    
+
     return (
         <div className="page-content">
             <div>This is the homepage</div>
--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -6,13 +6,13 @@ const NavBar = (props) => {
     return (
         <div className="nav">
             <ul className="navBar">
-                <li> <Link to="/">Home</Link> </li>
+                <li className="navRegEl"> <Link to="/">Home</Link> </li>
                 {/* <li> <Link to="/game">Game</Link> </li> */}
-                <li> <Link to="/charts">Charts</Link> </li>
-                <li className={props.loggedIn ? "hidden right" : "right"}> <Link  to="/login"> Login </Link>  </li>
-                <li className={props.loggedIn ? "right" : "right hidden"}> <Link  to="/profile"> My Profile </Link> </li>
-                <li className={props.loggedIn > 1 ? "" : "hidden"}> <Link to="/admin" > Admin </Link> </li>
-                <li className={props.loggedIn ? "" : "hidden"}> <Link onClick={()=>{props.setLoggedIn(0)}} to="/" > Logout </Link> </li>
+                <li className="navRegEl"> <Link to="/charts">Charts</Link> </li>
+                <li className={props.loggedIn ? "hidden right" : "navRegEl"}> <Link  to="/login"> Login </Link>  </li>
+                <li className={props.loggedIn ? "navRegEl" : "right hidden"}> <Link  to="/profile"> My Profile </Link> </li>
+                <li className={props.loggedIn > 1 ? "navRegEl" : "hidden"}> <Link to="/admin" > Admin </Link> </li>
+                <li className={props.loggedIn ? "navRegEl" : "hidden"}> <Link onClick={()=>{props.setLoggedIn(0)}} to="/" > Logout </Link> </li>
 			</ul>
 
             <input type="search" placeholder="Search for games or users" className="game-search-bar"/>
