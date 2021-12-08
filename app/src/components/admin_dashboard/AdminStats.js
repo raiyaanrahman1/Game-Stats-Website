f6f02f2f3e775d144c6cd768d8bbfd3fee91a96e
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2021-12-07 20:02:35 -0500

    changes to admin dashboard

--- app/src/components/admin_dashboard/AdminStats.js
+++ app/src/components/admin_dashboard/AdminStats.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AdminStats() {
 	// TODO: Get stats from server.
-	const userCount = 2
-	const newUserCount = 2
-	const gameCount = 20
-	const newGameCount = 3
+	const [userCount, setUserCount] = useState(0);
+	// const newUserCount = 2
+	const [gameCount, setGameCount] = useState(0);
+	// const newGameCount = 3
 
-	const visitCount = 1;	// Don't know how to get this info, maybe through external library?
-	const newVisitCount = 1;
+	// const visitCount = 1;	// Don't know how to get this info, maybe through external library?
+	// const newVisitCount = 1;
+
+	fetch("api/games").then(res => {
+		if(res.ok){
+			return res.json();
+		}
+	}).then(json => {
+		if(json.games && json.games.length > -1){
+			setGameCount(json.games.length);
+		}
+		
+	});
+
+	fetch("api/users").then(res => {
+		if(res.ok){
+			return res.json();
+		}
+	}).then(json => {
+		if(json.length && json.length > -1){
+			setUserCount(json.length);
+		}
+	})
 
 	return (<div className="admin-settings admin-stats">
 		<p>Users Count: {userCount} <br/>
-			&emsp;+{newUserCount} new users today</p>
+			{/* &emsp;+{newUserCount} new users today  */} </p>
 		<p>Games Count: {gameCount} <br/>
-			&emsp;+{newGameCount} new games today</p>
-		<p>Visiter Count: {visitCount} <br/>
-			&emsp;+{newVisitCount} new visiter today</p>
+			{/* &emsp;+{newGameCount} new games today */}
+			</p>
+		{/* <p>Visiter Count: {visitCount} <br/>
+			&emsp;+{newVisitCount} new visiter today</p> */}
 		
 		{/* maybe add line graphs below ? */}
 		
--- app/src/components/admin_dashboard/AdminUsers.js
+++ app/src/components/admin_dashboard/AdminUsers.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
 const searchUsers = (e) => {
   e.preventDefault();
   //search games
+  let username = document.getElementById("user-search-input").textContent;
+  
 };
 
 function AdminUsers() {
+  let [users, setUsers] = useState([]);
+  let [username, setUsername] = useState("placeholder username");
+  let [description, setDescription] = useState("placeholder description");
+  fetch("api/users").then(res => {
+    if(res.ok){
+      return res.json();
+    }
+  }).then(users => {
+    setUsers(users);
+  })
   return (
     <div className="admin-settings">
-      <form className="admin-users-search" onSubmit={searchUsers}>
+      <form className="admin-users-search" onSubmit={(e)=> {
+        e.preventDefault();
+        //search games
+        let username = document.getElementById("user-search-input").value;
+        console.log(username);
+        // console.log(e.target);
+        for(let user of users){
+          if(user.username === username){
+            setUsername(user.username);
+            setDescription(user.description);
+          }
+        }
+      }}>
         <label> Search Users: {/*We'd get the id here*/} </label>
-        <input placeholder="Please Enter a Username" />
+        <input id="user-search-input" placeholder="Please Enter a Username" />
       </form>
       <div className="admin-cur-user-display">
         <div className="admin-profile-display-left">
           <div className="admin-user-display-profile-image"></div>
           <div className="profile-name-desc">
-            <h4> Example User </h4>
-            <h5> Example User Description</h5>
+            <h4> {username} </h4>
+            <h5> {description} </h5>
           </div>
         </div>
         <div>
